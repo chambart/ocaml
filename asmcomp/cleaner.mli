1af1bd4da93c845ad6a4b9fcd29ed38dcefebb23
Pierre Chambart <pierre.chambart@ocamlpro.org>
2013-05-27 19:29:00 +0200

    Change inlining strategy, do 3 loops

--- asmcomp/cleaner.mli
+++ asmcomp/cleaner.mli
@@ -4,6 +4,18 @@ val clean : Flambdainfo.analysis_result ->
   ExprSet.t -> ExprSet.elt flambda -> ExprSet.elt flambda
 (** [clean value_analysis unpure_expressions expression] *)
 
-val inlining : Flambdainfo.analysis_result ->
+type inlining_kind =
+  | Minimal
+  | With_local_functions
+
+val inlining : inlining_kind -> Flambdainfo.analysis_result ->
   ExprSet.elt flambda -> ExprSet.elt flambda
 (** [inlining value_analysis expression] *)
+
+val specialise : Flambdainfo.analysis_result ->
+  ExprSet.t -> ExprSet.elt flambda -> ExprSet.elt flambda
+
+val rebind : Flambdainfo.analysis_result ->
+  ExprSet.t -> ExprSet.elt flambda -> ExprSet.elt flambda
+
+val remove_unused_closure_param : 'a flambda -> 'a flambda
