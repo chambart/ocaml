7e0fe034ab72ee21850a3ae1eb601c50920a8182
Pierre Chambart <pierre.chambart@ocamlpro.org>
2013-10-17 12:29:30 +0200

    Improve comment on constant.mli

--- asmcomp/constants.mli
+++ asmcomp/constants.mli
@@ -19,4 +19,5 @@ val not_constants : for_clambda:bool -> 'a Flambda.flambda -> constant_result
 (** [not_constant ~for_clambda expr]
     If for_clambda is true, are marked constant only expressions that can
     effectively be compiled to constants by Clambdagen.
-    i.e. field access to a constant are not considered constant *)
+    When for_clambda is false, field access to a constant are not considered
+    constant *)
