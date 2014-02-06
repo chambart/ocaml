4fe30d1e2bd7cf07531f6e0805411779480d6ed8
Pierre Chambart <pierre.chambart@ocamlpro.org>
2014-02-06 12:16:48 +0100

    Fix 1 : Fix copyright year

--- asmcomp/printflambda.ml
+++ asmcomp/printflambda.ml
@@ -5,7 +5,7 @@
 (*            Xavier Leroy, projet Cristal, INRIA Rocquencourt         *)
 (*                     Pierre Chambart, OCamlPro                       *)
 (*                                                                     *)
-(*  Copyright 2013 Institut National de Recherche en Informatique et   *)
+(*  Copyright 2014 Institut National de Recherche en Informatique et   *)
 (*  en Automatique.  All rights reserved.  This file is distributed    *)
 (*  under the terms of the Q Public License version 1.0.               *)
 (*                                                                     *)
